5ac71bed55d8699737a1d78d3af01dcb96479fd6
Adimir Colen <adimircolen@gmail.com>
2017-03-31 07:42:11 -0300

    all

--- src/br/ufmg/dcc/Acervo.java
+++ src/br/ufmg/dcc/Acervo.java
@@ -7,7 +7,7 @@ package br.ufmg.dcc;
 public class Acervo {
 	public Obra[] obras = new Obra[1];
 	
-	public void adicionarObra(Obra obra) {
+	public void adicionar(Obra obra) {
 		int length = this.obras.length;
 		if (obras[length-1] == null) {
 			obras[length-1] = obra;
@@ -19,8 +19,11 @@ public class Acervo {
 		}
 	}
 	
+	public Obra remover(String name, String autor, int tipo) {
+		return new Obra();
+	}
+	
 	public static void listar(Obra[] obras) {
-		System.out.println("Listagem do Acervo");
 		for(int i=0;i<obras.length;i++){
 			System.out.println(obras[i]);
 		}
@@ -37,7 +40,7 @@ public class Acervo {
 		Acervo porAutor = new Acervo();
 		for(int i=0;i<this.obras.length;i++){
 			if (this.obras[i] != null && this.obras[i].getAutor().equals(autor)) {
-				porAutor.adicionarObra(this.obras[i]);
+				porAutor.adicionar(this.obras[i]);
 			}
 		}
 		Acervo.listar(porAutor.obras);
@@ -47,7 +50,7 @@ public class Acervo {
 		Acervo esculturas = new Acervo();
 		for(int i=0;i<this.obras.length;i++){
 			if (this.obras[i] != null && this.obras[i].getTipo() == Obra.ESCULTURA) {
-				esculturas.adicionarObra(this.obras[i]);
+				esculturas.adicionar(this.obras[i]);
 			}
 		}
 		Acervo.listar(esculturas.obras);
@@ -57,12 +60,43 @@ public class Acervo {
 		Acervo pinturas = new Acervo();
 		for(int i=0;i<this.obras.length;i++){
 			if (this.obras[i] != null && this.obras[i].getTipo() == Obra.PINTURA) {
-				pinturas.adicionarObra(this.obras[i]);
+				pinturas.adicionar(this.obras[i]);
 			}
 		}
 		Acervo.listar(pinturas.obras);
 	}
-	public void remover(Obra obra) {}
+
+	public void remover(Obra[] obras) {
+		for(int i=0;i<obras.length;i++){
+			System.out.println("Remover "+ i + " Obra " + obras[i]);
+			remover(obras[i]);
+		}
+	}
+	
+	public void remover(Obra obra) {
+		for(int i=0;i<this.obras.length;i++){
+			
+			if (obra != null && obra.equals(this.obras[i])){
+				System.out.println("SPLIT -> "+ i);
+				if (i>0){
+					Obra[] primeiraParte = new Obra[i];
+					System.out.println("NOVO TAMANHO -> "+ primeiraParte.length);
+					System.arraycopy(this.obras, 0, primeiraParte, 0, primeiraParte.length);
+					Acervo.listar(primeiraParte);
+					Obra[] segundaParte = new Obra[i];
+					int init = (this.obras.length - 1) - i;
+					System.out.println("SEGUNDA PARTE init -> "+ init);
+					System.arraycopy(this.obras, init, primeiraParte, 0, primeiraParte.length);
+					
+				}else{
+					Obra[] novoArray = new Obra[this.obras.length -1];
+					System.arraycopy(this.obras, 1, novoArray, 0, novoArray.length);
+					this.obras = novoArray;
+				}
+				break;
+			}
+		}
+	}
 	
 	public Obra[] procurarPor(String nome, String autor) {
 		return procurar(new Obra(nome, autor));
@@ -72,11 +106,11 @@ public class Acervo {
 		Acervo result = new Acervo();
 		for(int i=0;i<this.obras.length;i++){
 			if ( obra.getNome() != null && this.obras[i].getNome().equals( obra.getNome() ) ){
-				result.adicionarObra(this.obras[i]);
+				result.adicionar(this.obras[i]);
 				continue;
 			}
 			if ( obra.getAutor() != null && this.obras[i].getAutor().equals( obra.getAutor() ) ){
-				result.adicionarObra(this.obras[i]);
+				result.adicionar(this.obras[i]);
 				continue;
 			}
 		}
@@ -102,15 +136,27 @@ public class Acervo {
 		Acervo acervo = new Acervo();
 		
 		for(int i=0;i<initObras.length;i++) {
-			acervo.adicionarObra(initObras[i]);
+			acervo.adicionar(initObras[i]);
 		}
 		
+		System.out.println("Listagem de Todas as Obras");
 		acervo.listar();
-		acervo.listarPorAutor("Michelangelo");
-		acervo.listarPorEsculturas();
-		acervo.listarPorPinturas();
-		Obra[] pesquisa = acervo.procurarPor(null, "Michelangelo");
-		Acervo.listar(pesquisa);
+		
+//		System.out.println("Listagem das Obras de Michelangelo");
+		Obra[] michelangelo = acervo.procurarPor(null, "Michelangelo");
+		Acervo.listar(michelangelo);
+		
+//		System.out.println("Listagem das Esculturas");
+//		acervo.listarPorEsculturas();
+		
+//		System.out.println("Listagem das Pinturas");
+//		acervo.listarPorPinturas();
+		
+		acervo.remover(michelangelo);
+		System.out.println("Procurar e remover obras de Michelangelo");
+		acervo.listar();
+		
+
 //		acervo.listar();
 		
 		
